92d9abf6e46189f9c4e2e77f30cd8a597a500dfd
Soma Sekhar K <somasekhar.kuruva@trianz.com>
2021-09-19 12:45:49 +0530

    -o StrictHostKeyChecking=no

--- Jenkinsfile_tomcat.groovy
+++ Jenkinsfile_tomcat.groovy
@@ -7,8 +7,8 @@ pipeline {
 
         stage("Install tomcat") {
             steps {
-              sh """  scp -i /tmp/demo.pem tomcatinstall.sh ec2-user@${serverip}:/tmp/ 
-                      ssh -i /tmp/demo.pem ec2-user@${serverip} " hostname -i && sudo chmod +x /tmp/tomcatinstall.sh && sudo bash /tmp/tomcatinstall.sh "
+              sh """  scp -o StrictHostKeyChecking=no -i /tmp/demo.pem tomcatinstall.sh ec2-user@${serverip}:/tmp/ 
+                      ssh -o StrictHostKeyChecking=no -i /tmp/demo.pem ec2-user@${serverip} " hostname -i && sudo chmod +x /tmp/tomcatinstall.sh && sudo bash /tmp/tomcatinstall.sh "
 
                 """ 
             }
