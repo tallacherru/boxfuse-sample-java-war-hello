9212051740ef5316ada07c12329288fb749adcdb
Soma Sekhar K <somasekhar.kuruva@trianz.com>
2021-09-19 10:59:14 +0530

    updated

--- Jenkisnfile_deploy.groovy
+++ Jenkisnfile_deploy.groovy
@@ -10,7 +10,7 @@ pipeline {
              ls -l
              pwd
              scp -i /tmp/demo.pem deployment.sh ec2-user@172.31.23.252:/tmp/
-             ssh -i /tmp/demo.pem ec2-user@172.31.23.252 "echo "Im in tomcat" && sudo chmod +x /tmp/deployment.sh && sudo bash /tmp/deployment.sh"
+             ssh -i /tmp/demo.pem ec2-user@172.31.23.252 "echo "Im in tomcat" && sudo chmod +x /tmp/deployment.sh && sudo bash /tmp/deployment.sh ${jobname} ${branch_name} ${build_number}"
 
              """
             }
--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,13 @@
 #1/bin/bash
+
+jobname = $1
+branch_name = $2
+build_number = $3
+
 aws s3 ls 
 ls -la
 hostname -i
 systemctl status tomcat
-aws s3 cp s3://artificats061/buildpipeline-git/master/15/hello-15.war /tmp/
-cp /tmp/hello-15.war /var/lib/tomcat/webapps
+#aws s3 cp s3://artificats061/buildpipeline-git/master/15/hello-15.war /tmp/
+aws s3 cp s3://artificats061/$jobname/$branch_name/$build_number/hello-$build_number.war /tmp/
+cp /tmp/hello-$build_number.war /var/lib/tomcat/webapps
