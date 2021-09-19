9d3a350ffb7ef5774628ccf62bce341295637118
Soma Sekhar K <somasekhar.kuruva@trianz.com>
2021-09-19 10:03:23 +0530

    added varible

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -9,7 +9,7 @@ pipeline {
 
             steps {
                 println "clone git code "
-                git branch: ${BRANCH_NAME},
+                git branch: "${BRANCH_NAME}",
                 url: 'https://github.com/KuruvaSomaSekhar/boxfuse-sample-java-war-hello.git'
 
                 sh "ls -lat"
@@ -24,7 +24,7 @@ pipeline {
         }
         stage("Upload artifacts to s3") {
             steps {
-                sh "aws s3 cp target/*.war s3://artificats061/$JOB_NAME/$BRANCH_NAME/$BUILD_NUMBER/"
+                sh "aws s3 cp target/*.war s3://artificats061/$JOB_NAME/${BRANCH_NAME}/$BUILD_NUMBER/"
             }
         }
     }
