20545837f4ece4c3226bd3bc07c3429bfcd063b9
Soma Sekhar K <somasekhar.kuruva@trianz.com>
2021-09-19 12:18:42 +0530

    updated ramu branch

--- deployment.sh
+++ deployment.sh
@@ -4,11 +4,12 @@ jobname="$1"
 branch_name="$2"
 build_number="$3"
 
+whoami
 
 aws s3 ls 
 ls -la
 hostname -i
 systemctl status tomcat
 #aws s3 cp s3://artificats061/buildpipeline-git/master/15/hello-15.war /tmp/
-aws s3 cp s3://artificats061/$jobname/$branch_name/$build_number/hello-$build_number.war /tmp/
-cp /tmp/hello-$build_number.war /var/lib/tomcat/webapps
+aws s3 cp s3://artificats061/$jobname/$branch_name/$build_number/hello-$build_number.war /var/lib/tomcat/webapps
+#cp /tmp/hello-$build_number.war /var/lib/tomcat/webapps
